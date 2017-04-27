tcwu2005/android-x86-platform-system-core

init: set default value of ro.hardware to the target name
People usually forget to set the value of androidboot.hardware to
the target name in kernel cmdline which results in boot failure.

The change tries to reduce such an error in the future. The target
name is hardcoded to the image. This eliminates the necessity of
androidboot.hardware.

init/Android.mk
@@ -38,6 +38,7 @@ include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_CPPFLAGS := $(init_cflags)
+LOCAL_CPPFLAGS += -DTARGET_PRODUCT=\"$(TARGET_PRODUCT)\"
 LOCAL_SRC_FILES:= \
     bootchart.cpp \
     builtins.cpp \

openthos/oto_system_core

init/Android.mk
@@ -18,6 +18,7 @@ LOCAL_SRC_FILES:= \
 	watchdogd.c
 
 LOCAL_CFLAGS    += -Wno-unused-parameter
+LOCAL_CFLAGS    += -DTARGET_PRODUCT=\"$(TARGET_PRODUCT)\"
 
 ifeq ($(strip $(INIT_BOOTCHART)),true)
 LOCAL_SRC_FILES += bootchart.c